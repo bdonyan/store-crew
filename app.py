75c43398ef046e202c8df9886b2c9cdee793f9f5
bdonyan <66976326+bdonyan@users.noreply.github.com>
2024-06-06 18:44:26 -0700

    Update app.py
    correct import

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 from crewai import Agent, Task, Crew, Process
-from crewai_tools.serper import SerperDevTool
+from crewai_tools import SerperDevTool
 
 app = Flask(__name__)
 CORS(app)
